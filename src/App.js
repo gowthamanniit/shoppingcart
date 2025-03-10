4dda8196556e07400afa8e87e59c358aa3cc3836
gowthamanniit <karurgowthaman@gmail.com>
2025-03-10 16:15:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {CartProvider} from 'react-use-cart'
+import { Home } from './Home'
+import Cart from './Cart'
+
+function App()
+{
+  return(
+    <>
+      <CartProvider>
+        <Home/>
+        <Cart/>
+      </CartProvider>
+    </>
+  )
+}
+export default App
---
+++ src/data.js
@@ -0,0 +1,30 @@
+import appleimg from './images/apple.jpg'
+import mangoimg from './images/mango.jpg'
+import pomegranateimg from './images/pomegranate.jpg'
+
+const data={
+    productData:[
+        {
+            id:1,
+            img:appleimg,
+            title:"Apple",
+            desc:"High QUality",
+            price:50
+            },
+        {
+            id:2,
+            img:mangoimg,
+            title:"Mango",
+            desc:"No. 1 Quality",
+            price:60            
+        },
+        {
+        id:3,
+        img:pomegranateimg,
+        title:"Pomegranate",
+        desc:"Indian Products",
+        price:70        
+        }
+    ]
+}
+export default data
